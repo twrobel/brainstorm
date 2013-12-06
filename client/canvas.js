758f52c8a7e390e167a4b77b4790db775bc1b2c0
Craig Wedseltoft <craig.wedseltoft@intelliware.ca>
2013-12-06 14:33:43 -0500

    draw shapes

--- client/canvas.js
+++ client/canvas.js
@@ -13,4 +13,29 @@ Template.main.rendered = function() {
 //    nodes.forEach(function (node) {
 //        context.fillRect(node.position[0], node.position[1], node.width, node.height);
 //    });
+	drawShapes();
 };
+
+function drawShapes() {
+	var shapes_cursor = Shapes.find();
+
+	var context = $("#mainCanvas")[0].getContext("2d");
+
+	debugger;
+
+	shapes_cursor.forEach(function(shape) {
+		context.beginPath();
+		context.lineWidth = '1';
+		context.strokeStyle = 'red';
+
+		var coords = shape.coords;
+
+		context.moveTo(coords[0].x, coords[0].y);
+
+		for(var i=1; i<coords.length; i++) {
+			context.lineTo(coords[i].x, coords[i].y);
+		}
+
+		context.stroke();
+	});
+}
