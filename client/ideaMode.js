8b8f493832aa9da17f02f542e25d021736bfee14
Graham Foster <fosterg88@gmail.com>
2013-12-06 13:36:32 -0500

    GF/TW: Capturing clicks on canvas

--- client/ideaMode.js
+++ client/ideaMode.js
@@ -4,3 +4,28 @@ Template.toolbar.events({
 		alert("Mode is: " + Session.get('mode'))
 	}
 })
+
+Template.main.events({
+	'click #mainCanvas': function(event, target){
+		var coord = extractClickCoordinates(event, target.firstNode);
+	}
+})
+
+function extractClickCoordinates(e, canvas){
+	var x;
+	var y;
+	if (e.pageX || e.pageY) {
+		x = e.pageX;
+		y = e.pageY;
+	}
+	else {
+		x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	}
+	x -= canvas.offsetLeft;
+	y -= canvas.offsetTop;
+	return {
+		x: x,
+		y: y
+	}
+}
