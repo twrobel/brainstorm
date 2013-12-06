47b97e5a5a723bc7a2b8e903178b8e8850ccf530
Caleb Powell <caleb.powell+github@gmail.com>
2013-12-06 14:14:19 -0500

    CP/MH: Trying to get canvas rendering to work... part 2

--- init-data.js
+++ init-data.js
@@ -1,3 +1,24 @@
 /**
  * Created by caleb on 12/6/2013.
  */
+
+IdeaNodes = new Meteor.Collection('IdeaNodes');
+IdeaEdges = new Meteor.Collection('IdeaEdges');
+Shapes = new Meteor.Collection('Shapes');
+
+if(Meteor.isServer && IdeaNodes.find().count() == 0) {
+    var ideaNode = {
+        position:[10,10],
+        width: 100,
+        height: 100,
+        text:'Sample Idea Node'
+    };
+
+    IdeaNodes.insert(ideaNode);
+}
+
+
+
+
+
+
