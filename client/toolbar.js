3f3ea89e394926c8296a850c18f1d35fb2ca9996
Craig Wedseltoft <craig.wedseltoft@intelliware.ca>
2013-12-06 16:23:29 -0500

    toolbar css

---
+++ client/toolbar.js
@@ -0,0 +1,7 @@
+Template.toolbar.isIdeaMode = function() {
+	return (Session.get('mode')==='idea') ? 'btn-primary' : 'btn-default';
+};
+
+Template.toolbar.isPencilMode = function() {
+	return (Session.get('mode')==='pencil') ? 'btn-primary' : 'btn-default';
+};
