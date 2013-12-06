afcd374dc9bac3f59f6c1202c8f03d7a74323cb4
Caleb Powell <caleb.powell+github@gmail.com>
2013-12-06 18:37:38 -0500

    MCH/CP - update to render the edges better... still not working

--- init-data.js
+++ init-data.js
@@ -16,16 +16,26 @@ if(Meteor.isServer && IdeaNodes.find().count() == 0) {
         position:[175, 125],
         text:'Sample Idea Node 2'
     };
+    var ideaNode3 = {
+        position:[500, 125],
+        text:'third ideaers'
+    };
 
 
     ideaNode1._id = IdeaNodes.insert(ideaNode1);
     ideaNode2._id = IdeaNodes.insert(ideaNode2);
+    ideaNode3._id = IdeaNodes.insert(ideaNode3);
 
     var ideaEdge = {
         node1: ideaNode1._id,
         node2: ideaNode2._id
     };
+    var ideaEdge2 = {
+        node1: ideaNode2._id,
+        node2: ideaNode3._id
+    };
     IdeaEdges.insert(ideaEdge);
+    IdeaEdges.insert(ideaEdge2);
 }
 
 
