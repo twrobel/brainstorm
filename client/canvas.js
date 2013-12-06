559fb3fc720d8f8f72ff4574f4376ca3cdda1de3
Caleb Powell <caleb.powell+github@gmail.com>
2013-12-06 14:32:31 -0500

    CP/MCH - update to make the render method get called on changes to the collection

--- client/canvas.js
+++ client/canvas.js
@@ -1,16 +1,23 @@
+Template.main.doNothing = function() {
+    return IdeaNodes.find().count();
+}
+
 Template.main.rendered = function() {
     var nodes = IdeaNodes.find();
     var context = $("#mainCanvas")[0].getContext("2d");
-    context.beginPath();
-    context.fillstyle = 'white';
-    context.rect(10,10,40,40);
-    context.fill();
     context.lineWidth = 2;
     context.strokeStyle = 'black';
-    context.stroke();
-//    context.fillStyle = 'white';
-//    context.strokeStyle = 'black';
-//    nodes.forEach(function (node) {
-//        context.fillRect(node.position[0], node.position[1], node.width, node.height);
-//    });
+    console.log("nodes:" + nodes.count());
+    nodes.forEach(function (node) {
+        console.log("render node");
+
+
+
+        console.log("x:" + node.position[0]);
+        console.log("y:" + node.position[1]);
+        console.log("w:" + node.width);
+        console.log("h:" + node.height);
+        context.rect(node.position[0], node.position[1], node.width, node.height);
+        context.stroke();
+    });
 };
