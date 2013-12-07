aca93010942527da29f9a31aa6da0fd9ded8c85f
Craig Wedseltoft <craig.wedseltoft@intelliware.ca>
2013-12-06 19:27:08 -0500

    sdfsdfsdfsd

--- client/toolbar.js
+++ client/toolbar.js
@@ -9,3 +9,23 @@ Template.toolbar.isIdeaMode = function() {
 Template.toolbar.isPencilMode = function() {
 	return (Session.get('mode')==='pencil') ? 'btn-primary' : 'btn-default';
 };
+
+Template.toolbar.isEraserMode = function() {
+	return (Session.get('mode')==='eraser') ? 'btn-primary' : 'btn-default';
+};
+
+Template.toolbar.showEraserButton = function() {
+	return (Session.get('mode')==='eraser' || Session.get('mode')==='pencil');
+};
+
+Template.toolbar.events({
+	'click #ideaModeSelector': function(){
+		Session.set('mode', 'idea');
+	},
+	'click #pencilModeSelector': function() {
+		Session.set('mode', 'pencil');
+	},
+	'click #erasorSelector': function() {
+		Session.set('mode', 'eraser');
+	}
+})
